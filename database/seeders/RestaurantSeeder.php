c8fd9785a08c31cf0df994569c1debc4af4fa224
user1 <user@email.com>
2023-09-04 16:55:29 +0200

    ristoranti nuovi 2

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -56,9 +56,51 @@ class RestaurantSeeder extends Seeder
                 'indirizzo' => 'Via Napoli, 567',
                 'partita_iva' => '56789012345',
                 'image' => 'immagine5.jpg',
+            ],
+            [
+                'email' => 'lacarbonara@carbonara.com',
+                'password' => bcrypt('lacarbonara'),
+                'nome_attività' => 'La Carbonara',
+                'indirizzo' => 'Via Roma, 123',
+                'partita_iva' => '12345678902',
+                'image' => 'immagine6.jpg',
+            ],
+            [
+                'email' => 'lapastafrolla@pastafrolla.com',
+                'password' => bcrypt('lapastafrolla'),
+                'nome_attività' => 'La Pastafrolla',
+                'indirizzo' => 'Piazza Venezia, 456',
+                'partita_iva' => '23456789013',
+                'image' => 'immagine7.jpg',
+            ],
+            
+            [
+                'email' => 'ilpiattoforte@ilpiattoforte.com',
+                'password' => bcrypt('ilpiattoforte'),
+                'nome_attività' => 'Il Piatto Forte',
+                'indirizzo' => 'Via Firenze, 789',
+                'partita_iva' => '34567890124',
+                'image' => 'immagine8.jpg',
+            ],
+            
+            [
+                'email' => 'ilpiattovegetariano@ilpiattovegetariano.com',
+                'password' => bcrypt('ilpiattovegetariano'),
+                'nome_attività' => 'Il Piatto Vegetariano',
+                'indirizzo' => 'Corso Vittorio Emanuele, 102',
+                'partita_iva' => '45678901235',
+                'image' => 'immagine9.jpg',
+            ],
+            [
+                'email' => 'ilpiattovegano@ilpiattovegano.com',
+                'password' => bcrypt('ilpiattovegano'),
+                'nome_attività' => 'Il Piatto Vegano',
+                'indirizzo' => 'Via Napoli, 568',
+                'partita_iva' => '56789012346',
+                'image' => 'immagine10.jpg',
             ]
         ];
-
+        
         Restaurant::insert($restaurants);
     }
 }
